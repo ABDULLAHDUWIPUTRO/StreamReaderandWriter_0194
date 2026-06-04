cc532f763d5e1fd46dfa0bbfa704b0066d384a6f
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2026-06-04 17:05:32 +0700

    menambahkan logika untuk membuka dan membaca file, serta menampilkan isi file atau pesan kesalahan jika file tidak ditemukan

--- ioFileWithPath..cpp
+++ ioFileWithPath..cpp
@@ -39,4 +39,19 @@ int main()
     //menunjuk ke sebuah file
     infile.open(Namafile + ".txt", ios::in);
 
+    cout << endl << ">=Membuka dan membaca file " << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
+    {
+        //perulangan untuk memunculkan setiap baris
+        while (getline(infile, baris)) 
+        {
+            cout << baris << endl;
+        }
+        //menutup file jika telah selesai 
+        infile.close();
+    }
+    //kondisi jika file tidak ada
+    else cout << "unable to open file";
+    return 0;
 }
